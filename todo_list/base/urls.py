8bc5781e7004e195e19e0193fc5d249eccce71bb
rameshTwilight <121216304+rameshTwilight@users.noreply.github.com>
2023-01-27 05:33:53 +0000

    django
    django

---
+++ todo_list/base/urls.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from .views import (
+    CustomLoginView,
+    RegisterPage,
+    TaskCreate,
+    TaskDelete,
+    TaskDetail,
+    TaskList,
+    TaskUpdate,
+)
+
+urlpatterns = [
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("register/", RegisterPage.as_view(), name="register"),
+    path("", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("task-create/", TaskCreate.as_view(), name="task_create"),
+    path("task-update/<int:pk>/", TaskUpdate.as_view(), name="task_update"),
+    path("task-delete/<int:pk>/", TaskDelete.as_view(), name="task_delete"),
+]
